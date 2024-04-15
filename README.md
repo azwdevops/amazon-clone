d9a86efb3e9f378fa7295d12fbb16aed80751d92
Zachary Waweru <azwdevops@gmail.com>
2024-04-15 10:21:44 +0300

    created amazon ui clone using html, css and javascript

---
+++ README.md
@@ -0,0 +1,15 @@
+- amazon UI made with HTML, CSS and Vanilla Javascript
+
+![alt text](amazon-1.png)
+
+![alt text](amazon-2.png)
+
+![alt text](amazon-3.png)
+
+![alt text](amazon-4.png)
+
+![alt text](amazon-5.png)
+
+![alt text](amazon-6.png)
+
+![alt text](amazon-7.png)
---
+++ main.js
@@ -0,0 +1,42 @@
+const imgs = document.querySelectorAll(".header-slider ul img");
+
+const prevButton = document.querySelector(".control-prev");
+const nextButton = document.querySelector(".control-next");
+
+let n = 0;
+
+function changeSlide() {
+  for (let i = 0; i < imgs.length; i++) {
+    imgs[i].style.display = "none";
+  }
+  imgs[n].style.display = "block";
+}
+
+changeSlide();
+
+prevButton.addEventListener("click", (e) => {
+  if (n > 0) {
+    n--;
+  } else {
+    n = imgs.length - 1;
+  }
+  changeSlide();
+});
+
+nextButton.addEventListener("click", () => {
+  if (n < imgs.length - 1) {
+    n++;
+  } else {
+    n = 0;
+  }
+  changeSlide();
+});
+
+const scrollContainer = document.querySelectorAll(".products");
+
+for (const item of scrollContainer) {
+  item.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    item.scrollLeft += e.deltaY;
+  });
+}
